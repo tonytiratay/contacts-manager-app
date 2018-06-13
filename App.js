bfa9f8d073379168ff021ae5688183a3057d92be
Tony Tiratay <tony.tiratay@gmail.com>
2018-06-13 03:02:25 +0200

    Add opacity buttons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, View, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { StatusBar, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { NativeRouter, Route, Link } from 'react-router-native';
 
 import LaunchScreen from './src/components/LaunchScreen';
@@ -11,17 +11,26 @@ export default class App extends React.Component {
       <NativeRouter>
         <View style={styles.container}>
           <StatusBar hidden={true}/>
-          <View style={styles.navContainer}>
-            <Link style={styles.navItem} to="/"><Text style={styles.navItemText}>Home</Text></Link>
-            <Link style={styles.navItem} to="/About"><Text style={styles.navItemText}>About</Text></Link>
+          <View style={styles.container}>
+            <Route path="/" exact component={ LaunchScreen } />
+            <Route path="/about" exact component={ About } />
           </View>
-          <Route path="/" exact component={ LaunchScreen } />
-          <Route path="/about" exact component={ About } />
+          <View style={styles.navContainer}>
+            <Link style={styles.navItemBorder} component={TouchableOpacity} to="/">
+              <Text style={styles.navItemText}>Home</Text>
+            </Link>
+            <Link style={styles.navItem} component={TouchableOpacity} to="/About">
+              <Text style={styles.navItemText}>About</Text>
+            </Link>
+          </View>
         </View>
       </NativeRouter>
     );
   }
 }
+
+const navColor = '#6bd3ea';
+const navItemColor = "#35b2ce"
 
 const styles = StyleSheet.create({
   container: {
@@ -35,15 +44,25 @@ const styles = StyleSheet.create({
     width: '100%',
     display: 'flex',
     flexDirection: 'row',
-    height: 70,
-    backgroundColor: '#1fa3c1'
+    height: 50,
+    backgroundColor: navColor,
   },
   navItem: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: navItemColor,
+  },
+  navItemBorder: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRightWidth: 1,
+    borderRightColor: '#1699b7',
+    backgroundColor: navItemColor,
   },
   navItemText: {
     color: '#fff',
   }
 }); 
+
