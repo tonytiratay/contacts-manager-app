ead5581f0118b270b9f3ef7cca779dd4df3bf6dd
Tony Tiratay <tony.tiratay@gmail.com>
2018-06-16 23:18:30 +0200

    add tests to reducers and actions

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
-import { Text } from 'react-native';
-import {store, persistor} from './src/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './src/store';
 
 import ContactApp from './ContactApp';
 
@@ -16,7 +15,6 @@ export default class App extends React.Component {
     return( 
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-        	<Text>Test</Text>
           <ContactApp />
         </PersistGate>
       </Provider>
